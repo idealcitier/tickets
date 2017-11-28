c3610458ae6d9624cb253ba7c7585521bae6bcda
leeguo <leeguo7@foxmail.com>
2017-11-28 19:42:50 +0800

    first commit

---
+++ get_info.py
@@ -0,0 +1,3 @@
+import requests
+
+url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=2017-11-27&leftTicketDTO.from_station=HZH&leftTicketDTO.to_station=SHH&purpose_codes=ADULT'
